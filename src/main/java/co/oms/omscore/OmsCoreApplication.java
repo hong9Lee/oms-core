f101a1bf72e667a8df3b3338d193ea14d0f3e342
hong9Lee <hongtemp9mac@gmail.com>
2026-02-28 16:34:07 +0900

    core init

---
+++ src/main/java/co/oms/omscore/OmsCoreApplication.java
@@ -0,0 +1,13 @@
+package co.oms.omscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/oms/omscore/OmsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.oms.omscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
